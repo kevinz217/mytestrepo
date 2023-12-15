fb632d60e579f74fd6d528b6d5c378cdf215f023
BT_1N3_05 <sample@bths.edu>
2023-12-15 09:05:23 -0500

    new method

--- src/NumberOperation.java
+++ src/NumberOperation.java
@@ -4,4 +4,8 @@ public class NumberOperation {
     public int theAddition(int num1, int num2) {
         return num1 + num2;
     }
+
+    public void sayNumber(int num) {
+        System.out.println("The number " + num + " is very cool");
+    }
 }
